06ed936b5451b15ee5e6bab0dbf8abd65fa966ab
Rotich <jane.rotich@student.moringaschool.com>
2023-12-28 13:43:53 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record) {
+    const topGame = record.find(game => game.result === "W");
+    return topGame ? topGame.year : undefined;
+  }
+  
+  // Example usage:
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ];
+  
+  const topYear = superbowlWin(record);
+  console.log(topYear); // Output: "2015"
+  
